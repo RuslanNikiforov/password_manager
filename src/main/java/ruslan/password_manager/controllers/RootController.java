940adb6e8444c4b41ef442a54132682687067999
RuslanNikiforov <koda260801@gmail.com>
2023-06-10 17:07:29 +0500

    trying to deploy

--- src/main/java/ruslan/password_manager/controllers/RootController.java
+++ src/main/java/ruslan/password_manager/controllers/RootController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class RootController {
 
-    @GetMapping("/koda/password-manager/")
+    @GetMapping("")
     public String root() {
         return "index";
     }
